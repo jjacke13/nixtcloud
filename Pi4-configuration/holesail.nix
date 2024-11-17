8fd4003638e0f824a1edb97bfeb2890c0f380bd1
Vaios K <jjacke@proton.me>
2024-11-17 21:39:43 +0200

    Update holesail.nix

--- Pi4-configuration/holesail.nix
+++ Pi4-configuration/holesail.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz") {} }:
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.buildNpmPackage rec {
   pname = "holesail";
