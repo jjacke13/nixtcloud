4935f7ba0349f9f1734cf9580d91f071f095f642
Vaios K <jjacke@proton.me>
2025-01-21 16:24:56 +0200

    Update flake.nix
    update to nixos 24.11
    now the flake uses nixos-unstable channel for nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,30 +1,35 @@
 {
   description = "Base system for raspberry pi 4";
   inputs = {
-    nixpkgs.url = "https://github.com/jjacke13/nixtcloud/releases/download/beta.2/nixos-nixtcloud.tar.gz";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    holesail.url = "github:jjacke13/holesail-nix";
   };
 
   outputs = { self, nixpkgs, nixos-generators, ... }@inputs:
   {
-    nixosModules = {
-      system = {
-        system.stateVersion = "24.05";
-      };  
-    };
     packages.aarch64-linux = {
-      sdcard = nixos-generators.nixosGenerate {
+      Rpi4 = nixos-generators.nixosGenerate {
         system = "aarch64-linux";
         format = "sd-aarch64";
+        specialArgs = { inherit inputs;};
         modules = [
           ./Rpi4/configuration.nix 
-          self.nixosModules.system
+          {system.stateVersion = "24.11";}
         ];
       };
     };
+    nixosConfigurations.nixtcloud = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs;};
+      modules = [
+        ./Rpi4/configuration.nix 
+        {system.stateVersion = "24.11";}
+      ];      
+    };
   };
 }
+
 
