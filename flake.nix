6907f2512c3417a714658c099d55cbe2bd0efdff
Vaios K <jjacke@proton.me>
2025-02-09 17:59:46 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,11 +3,7 @@
   
   #Nix-community cachix is needed if you want to build the image for raspberry pi 5. If you don't want to use it, 
   #the linux kernel will be built from source which takes a long time.
-  nixConfig = {
-      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
-	    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-  };
-  
+    
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-generators = {
