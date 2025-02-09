79c594475dd76b617468bac4cafaa611eb735585
Vaios K <jjacke@proton.me>
2025-02-09 20:57:10 +0200

    Add files via upload

--- Rpi4/hardware-configuration.nix
+++ Rpi4/hardware-configuration.nix
@@ -3,6 +3,19 @@
 { config, lib, pkgs, ... }:
 
 {
+  hardware.enableRedistributableFirmware = true;
+  
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = lib.mkDefault true;
+  
+  boot.initrd.availableKernelModules = [
+      "usbhid"
+      "usb_storage"
+      "vc4"
+      "pcie_brcmstb" # required for the pcie bus to work
+      "reset-raspberrypi" # required for vl805 firmware to load
+  ];
+  
   fileSystems."/" =
     { device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
