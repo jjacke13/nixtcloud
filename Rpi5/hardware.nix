e7b89f875683c9ce4beb28aac9d0a8750a5090dd
jjacke13 <jjacke@proton.me>
2025-02-18 20:39:24 +0200

    .

--- Rpi5/hardware.nix
+++ Rpi5/hardware.nix
@@ -4,6 +4,11 @@
 
 {
   raspberry-pi-nix.board = "bcm2712";
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
   
   security.rtkit.enable = true;
 
