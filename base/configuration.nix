e1fcd72b58829ae26a6db1044ff9e08fcaea2c06
jjacke13 <jjacke@proton.me>
2025-02-27 18:26:38 +0200

    .

--- base/configuration.nix
+++ base/configuration.nix
@@ -20,7 +20,6 @@ in
   nix.settings = {
 	  experimental-features = "nix-command flakes";
 	  auto-optimise-store = true;
-    #require-sigs = false;
     substituters = [ "https://nix-community.cachix.org" ];
 	  trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
   };
