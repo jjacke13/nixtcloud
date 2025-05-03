8fb86697624bdec8ac8dcfb19aa8e4f7c7110211
jjacke13 <jjacke@proton.me>
2025-05-03 13:48:17 +0300

    nextcloud 31

--- base/nextcloud.nix
+++ base/nextcloud.nix
@@ -12,7 +12,7 @@ in
 
   services.nextcloud = {
         enable = true;
-        package = pkgs.nextcloud30;
+        package = pkgs.nextcloud31;
         hostName = name;
         database.createLocally = true;
         config = {
